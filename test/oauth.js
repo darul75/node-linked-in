77fd03ab39707194911ce0d39786faf378a4dcac
julien <darul75@gmail.com>
2014-12-29 22:01:22 +0100

    update test wrap fn

--- test/oauth.js
+++ test/oauth.js
@@ -60,7 +60,6 @@ app.get(/^\/linked-callback\/?$/, function (req, res) {
     res.send(200);
     return;
   }
-
   oauth.getOAuthAccessToken(
     req.query.code, 
     { 
@@ -76,9 +75,7 @@ app.get(/^\/linked-callback\/?$/, function (req, res) {
         res.end(err + "");
         return;
       }
-
       accessToken = access_token;
-
       // authenticate linkedin API
       linkedin.authenticate({
         type: "oauth",
@@ -88,137 +85,89 @@ app.get(/^\/linked-callback\/?$/, function (req, res) {
     });
 });
 
+var testFnTmpl = function(fn, params, cb) {
+  fn(params, function(err, result) {
+    if (err)
+      result = {"error" : err};
+    //result['_NAME'] = fn.name;
+    cb(null, result);
+  });
+}
+
 app.get('/people', function (req, res) {
-
   var q = queue(1);
-
-  var ignoreError = function (task, callback) {
+  var ignoreError = function (task, cb) {
     task(function(error, result) {
-      return callback(null, result); // ignore error
+      return cb(null, result); // ignore error
     });
   };
-
-  var peopleCurrent = function(callback) {
-    linkedin.people.getCurrent({"url-field-selector": ':(id,first-name,last-name,industry,connections,group-memberships,educations,date-of-birth,positions,public-profile-url)'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.people.get';
-      callback(null, result);
-    });    
+  var peopleCurrent = function(cb) {
+    testFnTmpl(linkedin.people.getCurrent, {"url-field-selector": ':(id,first-name,last-name,industry,connections,group-memberships,educations,date-of-birth,positions,public-profile-url)'}, cb);    
   };
-
-  var peopleMemberByUrl = function(callback) {
-    linkedin.people.getMember({"url": 'https://www.linkedin.com/pub/julien-valery/70/624/b54', 'secure-urls': true, "url-field-selector": ':(headline,first-name,last-name)'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.people.get';
-      callback(null, result);
-    });    
+  var peopleMemberByUrl = function(cb) {
+    testFnTmpl(linkedin.people.getMember, {"url": 'https://www.linkedin.com/pub/julien-valery/70/624/b54', 'secure-urls': true, "url-field-selector": ':(headline,first-name,last-name)'}, cb);
   };
-
-  var peopleMemberById = function(callback) {
-    linkedin.people.getMember({"id": '2JVl6yrion', 'secure-urls': true,"url-field-selector": ':(headline,first-name,last-name)'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.people.getMember';
-      callback(null, result);
-    });    
+  var peopleMemberById = function(cb) {
+    testFnTmpl(linkedin.people.getMember, {"id": '2JVl6yrion', 'secure-urls': true,"url-field-selector": ':(headline,first-name,last-name)'}, cb);
   };
-
-  var peopleCurrentConnections = function(callback) {
-    linkedin.people.getCurrentConnections({"url-field-selector": ':(headline,first-name,last-name)'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.people.getCurrentConnections';
-      callback(null, result);
-    });    
+  var peopleCurrentConnections = function(cb) {
+    testFnTmpl(linkedin.people.getCurrentConnections, {"url-field-selector": ':(headline,first-name,last-name)'}, cb);    
   };  
-
-  var peopleMemberConnectionsById = function(callback) {
-    linkedin.people.getMemberConnections({
-      "idOrUrl": '2JVl6yrion',
-      "start": 0,
-      "count": 5
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.people.peopleMemberConnectionsById';
-      callback(null, result);
-    });    
+  var peopleMemberConnectionsById = function(cb) {
+    testFnTmpl(linkedin.people.getCurrentConnections, { "idOrUrl": '2JVl6yrion', "start": 0,"count": 5}, cb);    
   };
-
-  var peopleMemberConnectionsByUrl = function(callback) {
-    linkedin.people.getMemberConnections({
-      "idOrUrl": 'https://www.linkedin.com/pub/julien-valery/70/624/b54',
+  var peopleMemberConnectionsByUrl = function(cb) {
+    testFnTmpl(linkedin.people.getMemberConnections, { "idOrUrl": 'https://www.linkedin.com/pub/julien-valery/70/624/b54',
       "modified":"new",
       "modified-since":1267401600000,
       "start": 0,
-      "count": 5
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.people.peopleMemberConnectionsByUrl';
-      callback(null, result);
-    });    
+      "count": 5}, cb);    
   };
+  q.defer(ignoreError, peopleCurrent);
+  q.defer(ignoreError, peopleMemberById);
+  q.defer(ignoreError, peopleMemberByUrl);
+  q.defer(ignoreError, peopleCurrentConnections);  
+  q.defer(ignoreError, peopleMemberConnectionsById);
+  q.defer(ignoreError, peopleMemberConnectionsByUrl);  
+  q.awaitAll(function(error, results) {     
+    if (error)
+      res.json(error);
+    res.json(results);
+  });
+});
 
-  var groupsCurrentMembership = function(callback) {
-    linkedin.groups.getMemberships({
-      "membership-state": "owner",
+app.get('/groups', function (req, res) {
+  var q = queue(1);
+  var ignoreError = function (task, cb) {
+    task(function(error, result) {
+      return cb(null, result); // ignore error
+    });
+  };  
+  var groupsCurrentMembership = function(cb) {
+    testFnTmpl(linkedin.groups.getMemberships, { "membership-state": "owner",
       "start": 0,
       "count": 5,
-      "url-field-selector": ":(group:(id,name,posts;count=5,site-group-url))"
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.getMemberships';            
-      callback(null, result);
-    });
+      "url-field-selector": ":(group:(id,name,posts;count=5,site-group-url))"}, cb);     
   };
-
-  var groupsCurrentMembershipSettings = function(callback) {
-    linkedin.groups.getMembershipDetail({
-      "group-id": "12435",          
-      "url-field-selector": ":(show-group-logo-in-profile,email-digest-frequency,email-announcements-from-managers,allow-messages-from-members,email-for-every-new-post)"
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.getMemberships';            
-      callback(null, result);
-    });
+  var groupsCurrentMembershipSettings = function(cb) {      
+    testFnTmpl(linkedin.groups.getMembershipDetail, { "group-id": "12435",          
+      "url-field-selector": ":(show-group-logo-in-profile,email-digest-frequency,email-announcements-from-managers,allow-messages-from-members,email-for-every-new-post)"}, cb);           
   };
-
-  var groupsOne = function(callback) {
-    linkedin.groups.getDetail({
-      "group-id": "12435",
-      "url-field-selector": ":(id,name,site-group-url,posts:(id,summary,creator))"      
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
+  var groupsOne = function(cb) {
+    testFnTmpl(linkedin.groups.getDetail, { "group-id": "12435",
+      "url-field-selector": ":(id,name,site-group-url,posts:(id,summary,creator))" }, cb);    
   };
-
-  var updateOne = function(callback) {
-    linkedin.groups.updateMembership({
-      "group-id": "12435",
+  var updateOne = function(cb) {
+    testFnTmpl(linkedin.groups.updateMembership, { "group-id": "12435",
       "data": {
         "membership-state": {
           "code": "member"
         }
-      }
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
+      } 
+    }, cb);    
   };
-
-  var updateOneFull = function(callback) {
-    linkedin.groups.updateMembershipFull({
-      "group-id": "12435",
+  var updateOneFull = function(cb) {
+    testFnTmpl(linkedin.groups.updateMembershipFull, { "group-id": "12435",
       "data": {
         "group": {
           "id": 12345
@@ -237,62 +186,23 @@ app.get('/people', function (req, res) {
         "email-digest-frequency": {
           "code" :"none"
         }
-      }
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
+      } 
+    }, cb);     
   };
-
-  var deleteOne = function(callback) {
-    linkedin.groups.remove({
-      "group-id": "12435"      
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
+  var deleteOne = function(cb) {
+    testFnTmpl(linkedin.groups.remove, { "group-id": "12435"  }, cb);    
   };
-
-  var getPosts = function(callback) {
-    linkedin.groups.getPosts({
-      "group-id": "12435"      
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
+  var getPosts = function(cb) {
+    testFnTmpl(linkedin.groups.getPosts, { "group-id": "12435"  }, cb);   
   };
-
-   var getMembershipPosts = function(callback) {
-    linkedin.groups.getMembershipPosts({
-      "group-id": "12435"      
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
+  var getMembershipPosts = function(cb) {
+    testFnTmpl(linkedin.groups.getMembershipPosts, { "group-id": "12435"  }, cb);    
   };
-
-   var getSuggestionPosts = function(callback) {
-    linkedin.groups.getSuggestionPosts({
-      "group-id": "12435"      
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
+  var getSuggestionPosts = function(cb) {
+    testFnTmpl(linkedin.groups.getSuggestionPosts, { "group-id": "12435"  }, cb);
   };
-
-  var addPost = function(callback) {
-    linkedin.groups.addPost({
-      "group-id": "12435",
+  var addPost = function(cb) {
+    testFnTmpl(linkedin.groups.addPost, { "group-id": "12435",
       "data": {
         "title": "test",
         "summary": "summary",
@@ -302,34 +212,19 @@ app.get('/people', function (req, res) {
           "title": "ti",
           "description": "des"
         }
-      }
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.groups.one';            
-      callback(null, result);
-    });
-  };
-
-  // use linkedin API            
-  // q.defer(ignoreError, peopleCurrent);
-  // q.defer(ignoreError, peopleMemberById);
-  // q.defer(ignoreError, peopleMemberByUrl);
-  // q.defer(ignoreError, peopleCurrentConnections);  
-  // q.defer(ignoreError, peopleMemberConnectionsById);
-  // q.defer(ignoreError, peopleMemberConnectionsByUrl);
-  // q.defer(ignoreError, groupsCurrentMembership);
-  // q.defer(ignoreError, groupsCurrentMembershipSettings);  
-  // q.defer(ignoreError, groupsOne);
-  // q.defer(ignoreError, updateOne);
-  // q.defer(ignoreError, updateOneFull);
-  // q.defer(ignoreError, deleteOne);
+      }  
+    }, cb);    
+  };   
+  q.defer(ignoreError, groupsCurrentMembership);
+  q.defer(ignoreError, groupsCurrentMembershipSettings);  
+  q.defer(ignoreError, groupsOne);
+  q.defer(ignoreError, updateOne);
+  q.defer(ignoreError, updateOneFull);
+  q.defer(ignoreError, deleteOne);
   q.defer(ignoreError, getPosts);
   q.defer(ignoreError, getMembershipPosts);
   q.defer(ignoreError, getSuggestionPosts);
-  q.defer(ignoreError, addPost);
-  
-  // q.defer(linkedin.groups.getMemberships, {});  
+  q.defer(ignoreError, addPost); 
   q.awaitAll(function(error, results) {     
     if (error)
       res.json(error);
@@ -340,121 +235,47 @@ app.get('/people', function (req, res) {
 });
 
 app.get('/company', function (req, res) {
-
   var q = queue(1);
-
   var ignoreError = function (task, callback) {
     task(function(error, result) {
       return callback(null, result); // ignore error
     });
   };
   
-  var companyCurrent = function(callback) {
-    linkedin.company.all({}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.all';
-      callback(null, result);
-    });
+  var companyCurrent = function(cb) {
+    testFnTmpl(linkedin.company.all, {}, cb);    
   };
-
-  var companyOneAndFields = function(callback) {
-    linkedin.company.get({
-      'company-id': 1337,
-      'url-field-selector': ':(id,name)'
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.companyOneAndFields';
-      callback(null, result);
-    });
+  var companyOneAndFields = function(cb) {
+    testFnTmpl(linkedin.company.get, {'company-id': 1337, 'url-field-selector': ':(id,name)'}, cb);    
   };
-
-  var companyOne = function(callback) {
-    linkedin.company.get({"company-id": 162479}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.one';
-      callback(null, result);
-    });
+  var companyOne = function(cb) {
+    testFnTmpl(linkedin.company.get, {'company-id': 1337}, cb);    
   };
-
-  var companyUniversalName = function(callback) {
-    linkedin.company.getByUniversalName({"universal-name": 'linkedin'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.oneByUniversalName';
-      callback(null, result);
-    });
+  var companyUniversalName = function(cb) {
+    testFnTmpl(linkedin.company.getByUniversalName, {"universal-name": 'linkedin'}, cb);    
   };
-
-  var companyEmailDomain = function(callback) {
-    linkedin.company.getByEmailDomain({"email-domain": 'linkedin.com'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.companyEmailDomain';
-      callback(null, result);
-    });
+  var companyEmailDomain = function(cb) {
+    testFnTmpl(linkedin.company.getByEmailDomain, {"email-domain": 'linkedin.com'}, cb);    
   };
-
-  var companyCurrentIsAdmin = function(callback) {
-    linkedin.company.all({'is-company-admin': true}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.all';
-      callback(null, result);
-    });
+  var companyCurrentIsAdmin = function(cb) {
+    testFnTmpl(linkedin.company.all, {'is-company-admin': true}, cb);    
   };
-
-  var companyMixed = function(callback) {
-    linkedin.company.all({'url-field-selector': '::(162479,universal-name=linkedin)'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.all';
-      callback(null, result);
-    });
+  var companyMixed = function(cb) {
+    testFnTmpl(linkedin.company.all, {'url-field-selector': '::(162479,universal-name=linkedin)'}, cb);    
   };
-
-  var companyOneUpdate = function(callback) {
-    linkedin.company.getUpdate({"company-id": 162479}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.oneUpdate';
-      callback(null, result);
-    });
+  var companyOneUpdate = function(cb) {
+    testFnTmpl(linkedin.company.getUpdate, {'company-id': 1337}, cb);    
   };
-
-  var companyOneUpdatePagination = function(callback) {
-    linkedin.company.getUpdate({"company-id": 162479, "start":0, "count":5}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.companyOneUpdatePagination';
-      callback(null, result);
-    });
+  var companyOneUpdatePagination = function(cb) {
+    testFnTmpl(linkedin.company.getUpdate, {'company-id': 1337, "start":0, "count":5}, cb);    
   };
-
-  var companyOneUpdateEventType = function(callback) {
-    linkedin.company.getUpdate({"company-id": 162479, 'event-type': 'status-update'}, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.oneUpdateEventType';
-      callback(null, result);
-    });
+  var companyOneUpdateEventType = function(cb) {
+    testFnTmpl(linkedin.company.getUpdate, {'company-id': 1337,'event-type': 'status-update'}, cb);    
   };
-
-  var companyOneUpdateComments = function(callback) {
-    linkedin.company.getUpdateComments({"company-id": 162479, 
-      'company-update-key': 'UPDATE-c162479-5953413562058702849',
-      'event-type': 'CMPY'
-    }, function(err, result) {
-      if (err)
-        result = {"error" : err};
-      result['_NAME'] = 'linkedin.company.companyOneUpdateComments';
-      callback(null, result);
-    });
+  var companyOneUpdateComments = function(cb) {
+    testFnTmpl(linkedin.company.getUpdate, {'company-id': 1337,'company-update-key': 'UPDATE-c162479-5953413562058702849',
+      'event-type': 'CMPY'}, cb);    
   };
-  
-  // use linkedin API
   q.defer(ignoreError, companyCurrent);
   q.defer(ignoreError, companyOne);
   q.defer(ignoreError, companyOneAndFields);
